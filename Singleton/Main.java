95c12577888105211e44d4ebc5354b74361b8c15
Ruoqing Chen <69215820+pocopokoMassage@users.noreply.github.com>
2022-01-24 23:07:06 -0500

    Singleton Pattern added

---
+++ Singleton/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start.");
+		Singleton obj1 = Singleton.getInstance();
+		Singleton obj2 = Singleton.getInstance();
+		if(obj1 == obj2)
+		{
+			System.out.println("obj1 and obj2 are same");
+		}
+		else
+		{
+			System.out.println("obj1 and obj2 are different");
+		}
+		System.out.println("End.");
+	}
+}
---
+++ Singleton/Singleton.java
@@ -0,0 +1,14 @@
+public class Singleton
+{
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton()
+	{
+		System.out.println("Singleton has been created");
+	}
+	
+	public static Singleton getInstance()
+	{
+		return singleton;
+	}
+}
